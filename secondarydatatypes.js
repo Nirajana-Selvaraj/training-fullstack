1afe991e503fafe72a7215bc5c03c320050db942
Nirajana-Selvaraj <nirajanasri2304@gmail.com>
2024-12-28 13:03:24 +0530

    Commits

---
+++ secondarydatatypes.js
@@ -0,0 +1,40 @@
+ //array
+ arr = [10,20,30,[40.5,50.6],"a","NIRAJANA",true]
+ console.log(arr);
+
+ //object type 1
+ obj={
+    "First name":"NIRAJANA",
+    "LAST NAME" : "SELVARAJ",
+    "AGE":20
+ }
+ console.log(obj);
+
+ //object type 2
+ obj2={}
+obj2["movie"]="Three"
+obj2["budget"]="100cr"
+obj2["boxoffice"]="500cr"
+obj2["release"]="Worldwide"
+
+
+
+ console.log(obj2["movie"]);
+ console.log(obj2.budget);
+ console.log(obj2.boxoffice);
+
+
+
+ //object type 3
+
+
+ obj3= new Object()
+ obj3["phone"]="iphone"
+ obj3["store"]="App Store"
+ obj3["game1"]="Sudoku"
+ obj3["game2"]="Chess"
+ console.log(obj3.game1);
+ 
+
+
+
